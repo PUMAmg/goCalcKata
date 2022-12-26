1e027039febaa65f99ae412be0b9d78469f796f1
PUMAmg <puma.mw2@gmail.com>
2022-12-26 05:32:18 +0300

    New project

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Hello i am calculator. Right now I'll show 4 operations with arabic numbers:")
 	a, b := 8, 4
-	fmt.Printf("%d + %d = %d", a, b, a+b)
-	fmt.Printf("%d - %d = %d", a, b, a-b)
-	fmt.Printf("%d * %d = %d", a, b, a*b)
-	fmt.Printf("%d / %d = %d", a, b, a/b)
+	fmt.Printf("%d + %d = %d\n", a, b, a+b)
+	fmt.Printf("%d - %d = %d\n", a, b, a-b)
+	fmt.Printf("%d * %d = %d\n", a, b, a*b)
+	fmt.Printf("%d / %d = %d\n", a, b, a/b)
 }
